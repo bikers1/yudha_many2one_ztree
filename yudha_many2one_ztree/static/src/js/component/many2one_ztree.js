7750fa83b56172c585fcb4b77edab36187322b93
albertus restiyanto pramayudha <xabre0010@gmail.com>
2023-10-06 05:28:35 +0700

    testing 5

--- yudha_many2one_ztree/static/src/js/component/many2one_ztree.js
+++ yudha_many2one_ztree/static/src/js/component/many2one_ztree.js
@@ -4,10 +4,6 @@
 import { registry } from "@web/core/registry";
 import { Many2OneField } from "@web/views/fields/many2one/many2one_field";
 
-export class FieldZTreeMany2one extends Many2OneField {
-    setup() {
-        super.setup();
-    }
-}
+export class FieldZTreeMany2one extends Many2OneField {}
 FieldZTreeMany2one.additionalClasses = ['o_field_many2one'];
 registry.category("fields").add("ztree_select", FieldZTreeMany2one);
--- yudha_many2one_ztree/static/src/js/component/one2many_ztree.js
+++ yudha_many2one_ztree/static/src/js/component/one2many_ztree.js
@@ -4,10 +4,6 @@
 import { registry } from "@web/core/registry";
 import { X2ManyField } from "@web/views/fields/x2many/x2many_field";
 
-export class FieldZTreeChart extends X2ManyField {
-    setup() {
-        super.setup();
-    }
-}
+export class FieldZTreeChart extends X2ManyField {}
 FieldZTreeChart.additionalClasses = ['o_field_one2many'];
 registry.category("fields").add("ztree_chart", FieldZTreeChart);
